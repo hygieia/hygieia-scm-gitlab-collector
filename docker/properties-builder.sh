aa66c69be532c6c36225948591ac84a9c9a6e5c5
Yann Schepens <yann.schepens@gmail.com>
2020-08-17 14:43:10 +0200

    refactor: reuse same env var as sonar collector docker properties builder configuration

--- docker/properties-builder.sh
+++ docker/properties-builder.sh
@@ -32,19 +32,11 @@ echo "MONGODB_PORT: $MONGODB_PORT"
 
 cat > $PROP_FILE <<EOF
 #Database Name
-dbname=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_DATABASE:-dashboarddb}
-
-#Database HostName - default is localhost
-dbhost=${MONGODB_HOST:-10.0.1.1}
-
-#Database Port - default is 27017
+dbname=${MONGODB_DATABASE:-dashboarddb}
+dbhost=${MONGODB_HOST:-db}
 dbport=${MONGODB_PORT:-27017}
-
-#Database Username - default is blank
-dbusername=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_USERNAME:-dashboarduser}
-
-#Database Password - default is blank
-dbpassword=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_PASSWORD:-dbpassword}
+dbusername=${MONGODB_USERNAME:-dashboarduser}
+dbpassword=${MONGODB_PASSWORD:-dbpassword}
 
 #Collector schedule (required)
 gitlab.cron=${GITLAB_CRON:-0 0/5 * * * *}
