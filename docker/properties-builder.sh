cddb8d27c8ae3da176aee60d6dcaba9fc167b0ab
Yann Schepens <yann.schepens@gmail.com>
2020-08-17 14:47:43 +0200

    fix: remove useless and not consistent db configuration

--- docker/properties-builder.sh
+++ docker/properties-builder.sh
@@ -12,23 +12,12 @@ if [ "$TEST_SCRIPT" != "" ]
 then
   #for testing locally
   PROP_FILE=application.properties
-else 
-  PROP_FILE=/hygieia/config/application.properties
-fi
-  
-if [ "$MONGO_PORT" != "" ]; then
-	# Sample: MONGO_PORT=tcp://172.17.0.20:27017
-	MONGODB_HOST=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\1;'`
-	MONGODB_PORT=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\2;'`
 else
-	env
-	echo "ERROR: MONGO_PORT not defined"
-	exit 1
+  PROP_FILE=/hygieia/config/application.properties
 fi
 
 echo "MONGODB_HOST: $MONGODB_HOST"
 echo "MONGODB_PORT: $MONGODB_PORT"
-
 
 cat > $PROP_FILE <<EOF
 #Database Name
